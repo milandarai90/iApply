f0dbcadb9b042d70f4a5ada2a61a7186211878ec
milandarai90 <milandarai90@gmail.com>
2024-05-28 11:34:11 +0545

    little update

--- app/Http/Controllers/SuperadminAddController.php
+++ app/Http/Controllers/SuperadminAddController.php
@@ -12,6 +12,27 @@ class SuperadminAddController extends Controller
     }
     public function registerConsultancy(Request $request)
     {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'head_officeDistrict' => 'required',
+            'head_officeMunicipaity' => 'required',
+            'head_officeWard' => 'required',
+            'tel_number' => 'required',
+            'pan_number' => 'required',
+            'head_person_idcard' => 'required|image|mimes:jpeg,jpg,png,gif,webp',
+            'head_person_name' => 'required',
+            'head_person_number' => 'required',
+            'valid_documents' => 'required|image|mimes:jpeg,jpg,png,gif,webp',
+            'password' => 'required|min:8|max:30',
+            'c_password' => 'required|same:password',
+        ]);
+        $head_personID = time() . 'head_person_idcard' . $request->file('head_person_idcard')->getClientOriginalExtension();
+        $id_path = $request->file('head_person_idcard')->storeAs('public/head_person_idcard/' . $head_personID);
+        $id_newpath = str_replace('public/', '', $id_path);
 
+        $valid_documents = time() . 'valid_documents' . $request->file('valid_documents')->getClientOriginalExtension();
+        $valid_documents_path = $request->file('valid_documents')->storeAs('/public/valid_documents/' . $valid_documents);
+        $valid_newpath = str_replace('/public', '', $valid_documents_path);
     }
 }
