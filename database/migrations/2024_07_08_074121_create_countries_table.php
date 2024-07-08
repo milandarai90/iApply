d7d097977bf842a9644c4700530c5528a935353a
milandarai90 <milandarai90@gmail.com>
2024-07-08 14:08:19 +0545

    country migration done

---
+++ database/migrations/2024_07_08_074121_create_countries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('consultancy_id');
+            $table->unsignedBigInteger('branch_id');
+            $table->foreign('consultancy_id')->references('id')->on('consultancy_infos')->onDelete('cascade');
+            $table->foreign('branch_id')->references('id')->on('consultancy_branches')->onDelete('cascade');
+            $table->string('country_map');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
