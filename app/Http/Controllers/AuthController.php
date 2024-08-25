326aaf2f8c0e0177b9e22fc7f091373fe74f161e
milandarai90 <milandarai90@gmail.com>
2024-08-25 20:06:21 +0545

    only controllers and migration made for booking.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Mail\SendOtpMail;
 use App\Models\Otp;
-use App\Models\user;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Session;
