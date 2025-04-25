3a09201a78dbc0cf9dc6c113774345df6495fc7b
milandarai90 <milandarai90@gmail.com>
2025-04-26 01:08:57 +0545

    webhook implementation

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,25 @@ Route::middleware('auth:sanctum')->post('/cancelBooking', [BookingApiController:
 Route::middleware('auth:sanctum')->get('/notifications', [NotificationController::class, 'notifications']);
 
 
+Route::post('/github/webhook', function(){
+    try {
+        $secret = "iapply@2025";
+        $payload = file_get_contents("php://input");
+        // file_put_contents("webhook_request.log", $payload, FILE_APPEND);
+        $signature = $_SERVER["HTTP_X_HUB_SIGNATURE_256"] ?? "";
+        $hash = "sha256=" . hash_hmac("sha256", $payload, $secret);
+        if (!hash_equals($hash, $signature)) {
+            http_response_code(403);
+            exit("Invalid Signature");
+        }
 
+        $data = json_decode($payload, true);
+        if ($data["ref"] === "refs/heads/main") {
+            exec("cd ~/public_html/iapply && git pull origin main 2>&1", $output, $returnCode);
+            // file_put_contents("webhook.log", implode('\n', $output), FILE_APPEND);
+        }
+        return response()->json('success', 200);
+    } catch (Exception $e) {
+        return response()->json($e->getMessage(), 500);
+    }
+});
